1577ae03be9c69819f80d785aa5a6a9c9faffc63
Ragif <rafigrm@code.edu.az>
2022-07-07 00:18:33 +0400

    2ciTask

---
+++ 2ciTask/test1/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    public class Car
+    {
+        public string brand;
+        public ushort price;
+        public short speed;
+        public string color;
+
+        public Car()
+        {
+
+        }
+
+        public Car(string brand, ushort price, short speed, string color)
+        {
+            this.brand = brand;
+            this.price = price;
+            this.speed = speed;
+            this.color = color;
+        }
+
+        public string GetDetails()
+        {
+            return $"{brand} {price} {speed} {color}";
+        }
+    }
+}
---
+++ 2ciTask/test1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Task
+{
+
+    public class Task
+    {
+        static void Main()
+        {
+            BMW bmw = new BMW();
+            Mercedes mercedes = new Mercedes();
+
+            bmw.brand = "M5 F10";
+            bmw.price = 35000;
+            bmw.speed = 330;
+            bmw.color = "Black";
+
+            mercedes.brand = "S63 AMG";
+            mercedes.price = 45000;
+            mercedes.speed = 300;
+            mercedes.color = "White";
+
+            Console.WriteLine(bmw.GetDetails());
+            Console.WriteLine(mercedes.GetDetails());
+
+        }
+
+    }
+
+}
